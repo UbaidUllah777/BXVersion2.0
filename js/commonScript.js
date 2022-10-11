f58c8e2f9e4366a084285974feeec4b66e640df6
Ubaid <khanxx777@gmail.com>
2022-10-11 15:19:19 +0500

    ubaid

--- js/commonScript.js
+++ js/commonScript.js
@@ -6,6 +6,43 @@ const ElidHeaderLinkServices = document.getElementById("idHeaderLinkServices");
 const ElidHeaderLinkSrvOverView = document.getElementById(
   "idHeaderLinkSrvOverView"
 );
+const ElidHeaderLinkSrvMoApp = document.getElementById("idHeaderLinkSrvMobApp");
+const ElidHeaderLinkSrvWebApp = document.getElementById(
+  "idHeaderLinkSrvWebApp"
+);
+const ElidHeaderLinkSrvCustSWdev = document.getElementById(
+  "idHeaderLinkSrvCustSWdev"
+);
+const ElidHeaderLinkSrvSwQA = document.getElementById("idHeaderLinkSrvSwQA");
+const ElidHeaderLinkSrvUxUiDesign = document.getElementById(
+  "idHeaderLinkSrvUxUiDesign"
+);
+const ElidHeaderLinkSrvBlockchain = document.getElementById(
+  "idHeaderLinkSrvBlockchain"
+);
+const ElidHeaderLinkSrvCloudApp = document.getElementById(
+  "idHeaderLinkSrvCloudApp"
+);
+const ElidHeaderLinkSrvDataAnalytics = document.getElementById(
+  "idHeaderLinkSrvDataAnalytics"
+);
+const ElidHeaderLinkSrvTechConsult = document.getElementById(
+  "idHeaderLinkSrvTechConsult"
+);
+const ElidHeaderLinkSrvProjManag = document.getElementById(
+  "idHeaderLinkSrvProjManag"
+);
+const ElidHeaderLinkSrvCyberSec = document.getElementById(
+  "idHeaderLinkSrvCyberSec"
+);
+const ElidHeaderLinkOurWork = document.getElementById("idHeaderLinkOurWork");
+const ElidHeaderLinkCareers = document.getElementById("idHeaderLinkCareers");
+const ElidHeaderLinkContactUs = document.getElementById(
+  "idHeaderLinkContactUs"
+);
+const ElidHeaderTextReachUs = document.getElementById("idHeaderTextReachUs");
+const ElidHeaderTextPhone = document.getElementById("idHeaderTextPhone");
+
 //vendering Navbar Elements :End
 
 //Rendering Navbar Elements :start
@@ -20,5 +57,52 @@ ElidHeaderLinkAboutUs.href = `about-us.html`;
 
 // ElidHeaderLinkServices.textContent = `Services`;
 // ElidHeaderLinkServices.href = `services.html`;
+
+ElidHeaderLinkSrvMoApp.textContent = `Mobile Application`;
+ElidHeaderLinkSrvMoApp.href = `services-detail.html`;
+
+ElidHeaderLinkSrvWebApp.textContent = `Web App Development`;
+ElidHeaderLinkSrvWebApp.href = `services-detail.html`;
+
+ElidHeaderLinkSrvCustSWdev.textContent = `Custom Software Development`;
+ElidHeaderLinkSrvCustSWdev.href = `services-detail.html`;
+
+ElidHeaderLinkSrvSwQA.textContent = `Software QA`;
+ElidHeaderLinkSrvSwQA.href = `services-detail.html`;
+
+ElidHeaderLinkSrvUxUiDesign.textContent = `UX/UI Design`;
+ElidHeaderLinkSrvUxUiDesign.href = `services-detail.html`;
+
+ElidHeaderLinkSrvBlockchain.textContent = `Blockchain`;
+ElidHeaderLinkSrvBlockchain.href = `services-detail.html`;
+
+ElidHeaderLinkSrvCloudApp.textContent = `Cloud App Management`;
+ElidHeaderLinkSrvCloudApp.href = `services-detail.html`;
+
+ElidHeaderLinkSrvDataAnalytics.textContent = `Data Analytics and BI`;
+ElidHeaderLinkSrvDataAnalytics.href = `services-detail.html`;
+
+ElidHeaderLinkSrvTechConsult.textContent = `Technology Consultacy`;
+ElidHeaderLinkSrvDataAnalytics.href = `services-detail.html`;
+
+ElidHeaderLinkSrvProjManag.textContent = `Project Management`;
+ElidHeaderLinkSrvDataAnalytics.href = `services-detail.html`;
+
+ElidHeaderLinkSrvCyberSec.textContent = `Cyber Security`;
+ElidHeaderLinkSrvDataAnalytics.href = `services-detail.html`;
+
+ElidHeaderLinkOurWork.textContent = `Our Work`;
+ElidHeaderLinkOurWork.href = `blog.html`;
+
+ElidHeaderLinkCareers.textContent = `Careers`;
+ElidHeaderLinkCareers.href = `careers.html`;
+
+ElidHeaderLinkContactUs.textContent = `Contact Us`;
+ElidHeaderLinkContactUs.href = `contact.html`;
+
+ElidHeaderTextReachUs.textContent = `Reach BX Technologies`;
+
+ElidHeaderTextPhone.textContent = `+92-3159116035`;
+ElidHeaderTextPhone.href = `tel:+1234567890`;
 
 //Rendering Navbar Elements :start
