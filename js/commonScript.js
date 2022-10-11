945215e79ea34001470a9bef886b9338b43bad4d
Ubaid <khanxx777@gmail.com>
2022-10-11 14:28:31 +0500

    ubaid

--- js/commonScript.js
+++ js/commonScript.js
@@ -1,1 +1,24 @@
+//vendering Navbar Elements :start
 const ElidHeaderLogo = document.getElementById("idHeaderLogo");
+const ElidHeaderLinkHome = document.getElementById("idHeaderLinkHome");
+const ElidHeaderLinkAboutUs = document.getElementById("idHeaderLinkAboutUs");
+const ElidHeaderLinkServices = document.getElementById("idHeaderLinkServices");
+const ElidHeaderLinkSrvOverView = document.getElementById(
+  "idHeaderLinkSrvOverView"
+);
+//vendering Navbar Elements :End
+
+//Rendering Navbar Elements :start
+
+ElidHeaderLogo.src = `img/demos/it-services/logo.png`;
+
+ElidHeaderLinkHome.textContent = `Home`;
+ElidHeaderLinkHome.href = `index.html`;
+
+ElidHeaderLinkAboutUs.textContent = `About Us`;
+ElidHeaderLinkAboutUs.href = `about-us.html`;
+
+// ElidHeaderLinkServices.textContent = `Services`;
+// ElidHeaderLinkServices.href = `services.html`;
+
+//Rendering Navbar Elements :start
