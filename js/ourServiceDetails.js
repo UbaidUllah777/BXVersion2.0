a26130cee769ae085966311ea0e2ea45cd760342
Bxtech <shafiqpesh123@gmail.com>
2022-10-13 18:12:07 +0500

    shafiq

--- js/ourServiceDetails.js
+++ js/ourServiceDetails.js
@@ -880,6 +880,28 @@ const allStackServices = [
       // { image: "img/Techstackimg/ubuntu.png", serviceName: "ubuntu" },
     ],
   },
+
+  // ux-ui Design
+  {
+    id: "idServUxUiDesign",
+    serviceHeading: "UI-UX DESIGN TOOLS",
+
+    servicesDetails: "heading stack line foe ui-ux",
+    servicesImages: [
+      { image: "img/Techstackimg/photoshop.png", serviceName: "photoshop" },
+      { image: "img/Techstackimg/xd.png", serviceName: "adobe XD" },
+      {
+        image: "img/Techstackimg/illustrator.png",
+        serviceName: "adobe illustrator",
+      },
+      {
+        image: "img/Techstackimg/figma.png",
+        serviceName: "figma",
+      },
+      { image: "img/Techstackimg/sketching.png", serviceName: "sketch" },
+      // { image: "img/Techstackimg/ubuntu.png", serviceName: "ubuntu" },
+    ],
+  },
 ];
 //Service Stact Data :END
 
