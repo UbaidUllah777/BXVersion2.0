f01147a916f276d6b905d74e3eba7fe4c0e94f90
Ubaid <khanxx777@gmail.com>
2022-10-24 12:10:13 +0500

    ubaid

--- js/portfolio.js
+++ js/portfolio.js
@@ -5,6 +5,12 @@ const ElidHeadEducation = document.getElementById("idHeadEducation");
 const ElidMemberImg = document.getElementById("idMemberImg");
 const ElidMemerAboutMePara = document.getElementById("idMemerAboutMePara");
 const ElaboutMeMore = document.getElementById("aboutMeMore");
+const ELidAboutMe2ndPara = document.getElementById("idAboutMe2ndPara");
+const ELidAboutMe3rdPara = document.getElementById("idAboutMe3rdPara");
+const ElidSayHello = document.getElementById("idSayHello");
+const ElidMemberBlogsContainer = document.getElementById(
+  "idMemberBlogsContainer"
+);
 // const ElidMemberBirthday = document.getElementById("idMemberBirthday");
 // const ElidMembermarital = document.getElementById("idMembermarital");
 // const ElidMemberNationality = document.getElementById("idMemberNationality");
@@ -40,36 +46,45 @@ const AllTeamProfiles = [
     skype: "shafiqpesh123@gmail.com",
     phone: "+92-3312413434",
     email: "shafiq@bxtechnologies.net",
+    facebook: "N/A",
+    twitter: "",
+    linkedin: "",
+    blogAuthorName: "M.SHAFIQ",
 
     Experience: [
       {
-        expDesignation: "TC",
+        expDesignation: "Full Stack Software Developer ",
         expPara:
-          "za makkhki dher ghareeb wam aw za ba pa pindi Express k as a Tc wam bya me da Ghareeb tabah d videos wakaty aw khpal business m kolao ko aw ws dher zyaty paisy gatam aw mazy koma aw yawa masta Gf da, aw mazy p kom.",
-        expFrom: "Sep 2001",
-        expTo: "2017",
-        expCompany: "Pendi Express",
-        expCompanyDistination: "Pindi",
+          "Working as a software developer at BX Technologies Pvt Limited, Islamabad, Pakistan, My major responsibilities are developing databases and software applications in Javascript, React, C#, ASP.net and In MERN Stack. My additional Skills are HTML5, CSS, Bootsrap,Mediaquery, NODE, Mongoose and MYSQL",
+        expFrom: "FEB 2017",
+        expTo: "PRESENT",
+        expCompany: "BX TEchnologies Pvt Ltd.",
+        expCompanyDistination: "Islamabad",
       },
       {
-        expDesignation: "CTO",
-        expPara: "Bs nur sa owam kho gatam aw satam",
-        expFrom: "Oct 2019",
-        expTo: "Present",
-        expCompany: "BX Technologies Pvt Ltd.",
-        expCompanyDistination: "Blue Area,Islamabad, PK",
+        expDesignation: "Software Developer",
+        expPara:
+          "14 months of experience as a Desktop developer (C#, WPF)  & Web Developer (ASP.net) at ZeeSign Tech Pvt-Ltd Islamabad, Pakistan.",
+        expFrom: "NOV 2015",
+        expTo: "DEC 2016",
+        expCompany: "ZeeSign Tech Pvt Ltd.",
+        expCompanyDistination: "Islamabad, PK",
+      },
+      {
+        expDesignation: "Intern",
+        expPara:
+          "6 months of internship as a Desktop developer (C#, WPF) at Khyber Pakhtunkhwa Information and Technology Board Peshawar Pakistan.",
+        expFrom: "APR 2015",
+        expTo: "SEP 2015",
+        expCompany: "KPITB, Peshawar, Pakistan.",
+        expCompanyDistination: "Peshaawar, KPK",
       },
     ],
     EducationInDetails: [
       {
-        schoolName: "Cambridge Public High School",
-        degreeName: "SSC",
-        dates: "1996-2008",
-      },
-      {
-        schoolName: "Excelior College Sangota,Swat",
-        degreeName: "F.sc",
-        dates: "2008-2010",
+        schoolName: "Capital University of Science and Technology, Islamabad",
+        degreeName: "Master's of Computer Science",
+        dates: "2017-2019",
       },
       {
         schoolName: "University Of Agriculture, Peshawar",
@@ -77,19 +92,20 @@ const AllTeamProfiles = [
         dates: "2011-2015",
       },
       {
-        schoolName: "Capital University of Science and Technology, Islamabad",
-        degreeName: "Master's of Computer Science",
-        dates: "2017-2019",
+        schoolName: "Excelior College Sangota,Swat",
+        degreeName: "F.sc",
+        dates: "2008-2010",
+      },
+      {
+        schoolName: "Cambridge Public High School",
+        degreeName: "SSC",
+        dates: "1996-2008",
       },
     ],
     Skills: [
       {
         skillName: "JavaScript",
         skillPercentage: "80%",
-      },
-      {
-        skillName: "Php",
-        skillPercentage: "50%",
       },
       {
         skillName: "Asp.Net",
@@ -116,7 +132,7 @@ const AllTeamProfiles = [
         skillPercentage: "70%",
       },
       {
-        skillName: "C# WPF",
+        skillName: "C# & WPF",
         skillPercentage: "70%",
       },
       {
@@ -126,19 +142,19 @@ const AllTeamProfiles = [
     ],
     Languages: [
       {
-        languageImg: "",
         languageName: "ENGLISH",
-        languageAbility: "90%",
+        languageAbility: "Advanced%",
+        flag: "us",
       },
       {
-        languageImg: "",
         languageName: "URDU",
-        languageAbility: "100%",
+        languageAbility: "Advanced",
+        flag: "pk",
       },
       {
-        languageImg: "",
         languageName: "PASHTO",
-        languageAbility: "100%",
+        languageAbility: "Advanced",
+        flag: "pk",
       },
     ],
   },
@@ -150,7 +166,12 @@ const AllTeamProfiles = [
     designation: "Full Stack Developer & MD  at BX Technologies",
     previousDesignation: "Software Developer",
     headEducation: "Ms(CS)",
-    aboutMePara: "La lala lala lalaala lalalal ",
+    aboutMePara:
+      "It is my pleasure to introduce myself as a Full-stack software developer. I am passionate about web development, Desktop development, software testing, and API development. ",
+    aboutSecondPara:
+      "I have an experience in product development, who is capable of thriving in a fast-paced environment. Leadership capabilities, manages, motivates, and guides teams at all stages of project-based efforts. I am a  team player with excellent communication and management skills.",
+    aboutThirdPara:
+      "I have worked in many programming languages and frameworks based on the needs and requirements of the projects. My expertise is in JavaScript, ASP.net, C#, CSS, HTML, React,  MySQL and as a MERN Stack. ",
     phone: "+923165627477",
     birthday: "01/01/1993",
     marital: "Single",
@@ -158,6 +179,10 @@ const AllTeamProfiles = [
     skype: "N/A",
     phone: "+92-3165627477",
     email: "ubaid@bxtechnologies.net",
+    facebook: "N/A",
+    twitter: "https://twitter.com/SebLumber",
+    linkedin: "https://www.linkedin.com/in/ubaid-ullah-b3a185189/",
+    blogAuthorName: "UBAID ULLAH",
 
     Experience: [
       {
@@ -389,6 +414,8 @@ for (const SinglePortfolio of AllTeamProfiles) {
     `;
       ElidMemberImg.src = `${SinglePortfolio.HeadImg}`;
       ElidMemerAboutMePara.textContent = `${SinglePortfolio.aboutMePara}`;
+      ELidAboutMe2ndPara.textContent = `${SinglePortfolio.aboutSecondPara}`;
+      ELidAboutMe3rdPara.textContent = `${SinglePortfolio.aboutThirdPara}`;
       // ElidMemberBirthday.textContent = `${SinglePortfolio.birthday}`;
       // ElidMembermarital.textContent = `${SinglePortfolio.marital}`;
       // ElidMemberNationality.textContent = `${SinglePortfolio.nationality}`;
@@ -459,6 +486,103 @@ for (const SinglePortfolio of AllTeamProfiles) {
         <span class="font-weight-semibold custom-max-width-1 text-color-dark"><img src="img/blank.gif" class="flag flag-${singleLanguage.flag} me-1" alt="${singleLanguage.languageName}">${singleLanguage.languageName}:</span>
         <span class="custom-text-color-2">Advanced</span>
       </li>`;
+      }
+      ElidSayHello.innerHTML = `
+      <a href="mailto:${SinglePortfolio.email}" class="text-decoration-none">
+											<span class="feature-box custom-feature-box align-items-center mb-4">
+												<span class="custom-feature-box-icon">
+													<i class="icon-envelope icons text-color-light"></i>
+												</span>
+												<span class="feature-box-info">
+													<span class="custom-label font-weight-semibold text-uppercase custom-text-color-1 line-height-5">Email</span>
+													<strong class="font-weight-light text-color-light custom-opacity-effect-1">${SinglePortfolio.email}</strong>
+												</span>
+											</span>
+										</a>
+										<a href="tel:${SinglePortfolio.phone}" class="text-decoration-none">
+											<span class="feature-box custom-feature-box align-items-center mb-4">
+												<span class="custom-feature-box-icon">
+													<i class="icon-phone icons text-color-light"></i>
+												</span>
+												<span class="feature-box-info">
+													<span class="custom-label font-weight-semibold text-uppercase custom-text-color-1 line-height-5">Phone</span>
+													<strong class="font-weight-light text-color-light custom-opacity-effect-1">${SinglePortfolio.phone}</strong>
+												</span>
+											</span>
+										</a>
+										
+										<span class="feature-box custom-feature-box align-items-center">
+											<span class="custom-feature-box-icon">
+												<i class="icon-share icons text-color-light"></i>
+											</span>
+											<a href="${SinglePortfolio.facebook}" class="d-flex text-decoration-none">
+												<span class="feature-box-info">
+													<strong class="font-weight-light text-color-light custom-opacity-effect-1 ">Facebook</strong>
+												</span>
+											</a>
+											<a href="${SinglePortfolio.twitter} class="d-flex text-decoration-none ">
+												<span class="feature-box-info custom-both-side-border mt-2">
+													<strong class="font-weight-light text-color-light custom-opacity-effect-1 ">Twitter</strong>
+												</span>
+											</a>
+											<a href="${SinglePortfolio.linkedin}" class="d-flex text-decoration-none">
+												<span class="feature-box-info p-0 mt-2">
+													<strong class="font-weight-light text-color-light custom-opacity-effect-1">Linkedin</strong>
+												</span>
+											</a>
+										</span>
+      `;
+      for (const singleBlogPost of BxBlogPosts) {
+        if (singleBlogPost.blogAuthor == SinglePortfolio.blogAuthorName) {
+          ElidMemberBlogsContainer.innerHTML += ` <div class="pt-5 BxBlogPost col-lg-6 isotope-item text-start">
+        <article class="card custom-post-style-1 border-0">
+          <header class="overlay overlay-show">
+            <img
+              id="blogImg"
+              class="img-fluid"
+              src="${singleBlogPost.blogImg}"
+              alt="Blog Post Thumbnail 1"
+            />
+            <h4
+              class="font-weight-bold text-6 position-absolute bottom-0 left-0 z-index-2 ms-4 mb-4 pb-2 ps-2 pe-5 me-5"
+            >
+              <a
+                id="idBlogPostHeading"
+                href="blog-post.html"
+                class="text-color-light text-decoration-none"
+                >${singleBlogPost.BlogLongHeading}</a
+              >
+            </h4>
+          </header>
+          <div class="card-body">
+            <ul
+              class="list list-unstyled custom-font-secondary pb-1 mb-2"
+            >
+              <li
+                class="list-inline-item line-height-1 me-1 mb-0"
+              >
+                ${singleBlogPost.blogPostedOnDate}
+              </li>
+              <li
+                class="d-inline-block list-inline-item vertical-divider border-color-dark px-2 line-height-1 me-1"
+              >
+                ${singleBlogPost.blogAuthor}
+              </li>
+            </ul>
+            <p class="custom-text-size-1 mb-2">
+              ${singleBlogPost.blogDisplayPara}
+            </p>
+            <a
+              id="${singleBlogPost.id}"
+              onclick="FnClickedBlog('${singleBlogPost.id}')"
+              href="blog-post.html"
+              class="text-color-primary font-weight-bold text-decoration-underline custom-text-size-1"
+              >Read More...</a
+            >
+          </div>
+        </article>
+      </div>`;
+        }
       }
     }
   }
